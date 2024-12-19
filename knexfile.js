018d3a6cac73e0835171d51d25d6b6015545ef33
Garrett Wong <123900256+Gwong@users.noreply.github.com>
2024-12-19 00:41:37 -0500

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,10 @@ const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
+
 module.exports = {
   development: {
     client: "pg",
